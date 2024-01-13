3c9f4520bb583202ac0c27f0934d5ebfdb2f0972
Christian Attard <chris.att.91@gmail.com>
2024-01-13 13:39:01 +0100

    main class: insert sample text card class: bool variables for face position, constructors

---
+++ Clock_Patience/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock_Patience
+{
+    internal class Card
+    {
+        //boolean variable for card facing up or down
+        public bool faceUp { get; set; }
+        public bool faceDown { get; set; }
+
+        //default constructor
+        public Card() { }
+
+        //constructor with parameters 
+        public Card(bool faceUp, bool faceDown)
+        {
+            this.faceUp = faceUp;
+            this.faceDown = faceDown;   
+        }
+    }
+}
